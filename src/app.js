60fc844fc4810f4bc10d3c36d9c6e856a5f9f293
datkinson <95184929+datkinson52009@users.noreply.github.com>
2021-12-05 00:44:56 -0600

    Added weather icon from api

--- src/app.js
+++ src/app.js
@@ -22,23 +22,32 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data.weather[0].icon);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    "http://openweathermap.org/img/wn/" +
+      response.data.weather[0].icon +
+      "@2x.png"
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "462d1f08d569f95ec1f23ff00bbaacc6";
 let units = "metric";
-let city = "Dallas";
+let city = "Lawton";
 
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=" +
