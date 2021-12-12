0dad5ad8e43602b52ff17aeb8ba32e0bc01d6d33
datkinson <95184929+datkinson52009@users.noreply.github.com>
2021-12-11 20:32:10 -0600

    Added another day in forecast

--- src/app.js
+++ src/app.js
@@ -30,6 +30,41 @@ function formatDate(timestamp) {
   //return day + " " + hours + ":" + minutes;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = "";
+
+  forecastHTML =
+    forecastHTML +
+    `
+  <div class="row">
+  <div class="col-2">
+<div class="weather-forecast-day">Thurs <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="50px"></div>
+<span class ="weather-forecast-high">30°</span> 
+<span class="weather-forecast-low">0°</span>
+  </div>
+</div>
+</div>
+`;
+
+  forecastHTML =
+    forecastHTML +
+    `
+  <div class="row">
+  <div class="col-2">
+<div class="weather-forecast-day">Thurs <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="50px"></div>
+<span class ="weather-forecast-high">30°</span> 
+<span class="weather-forecast-low">0°</span>
+  </div>
+</div>
+</div>
+`;
+
+  forecastElement.innerHTML = forecastHTML;
+  //forecastElement.innerHTML + forecastElement.innerHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -100,93 +135,4 @@ search("Lawton");
 let form = document.querySelector("#temp-search");
 form.addEventListener("submit", updateCity);
 
-// let now = new Date();
-// let h3 = document.querySelector("h3");
-
-// let days = [
-//   "Sunday",
-//   "Monday",
-//   "Tuesday",
-//   "Wednesday",
-//   "Thursday",
-//   "Friday",
-//   "Saturday",
-// ];
-// let day = days[now.getDay()];
-// let hour = now.getHours();
-// let min = now.getMinutes();
-
-// h3.innerHTML = day + " " + hour + ":" + min;
-
-// function updateCity(event) {
-//   event.preventDefault();
-//   let now = new Date();
-//   let days = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//   ];
-//   let day = days[now.getDay()];
-//   let hour = now.getHours();
-//   let min = now.getMinutes();
-
-//   h3.innerHTML = day + " " + hour + ":" + min;
-
-//   let searchInput = document.querySelector("#city-name");
-
-//   let h1 = document.querySelector("h1");
-//   h1.innerHTML = searchInput.value;
-
-//   let apiKey = "462d1f08d569f95ec1f23ff00bbaacc6";
-//   let units = "metric";
-//   //let city = "Dallas";
-
-//   let apiUrl =
-//     "https://api.openweathermap.org/data/2.5/weather?q=" +
-//     searchInput.value +
-//     "&appid=" +
-//     apiKey +
-//     "&units=" +
-//     units;
-
-//   function showTemperature(response) {
-//     let currentTemp = document.querySelector(".currentTemp");
-//     currentTemp.innerHTML = Math.round(response.data.main.temp) + "°C";
-
-//     let low = document.querySelector(".low");
-//     low.innerHTML = "Low: " + Math.round(response.data.main.temp_min) + "°C";
-
-//     let wind = document.querySelector(".wind");
-//     wind.innerHTML = "Wind speed: " + response.data.wind.speed + "km/h";
-
-//     let humidity = document.querySelector(".humidity");
-//     humidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-//   }
-
-//   axios.get(apiUrl).then(showTemperature);
-// }
-
-// function showPosition(position) {
-//   let latitude = position.coords.latitude;
-//   let longitude = position.coords.longitude;
-//   let apiKey = "462d1f08d569f95ec1f23ff00bbaacc6";
-
-//   let currentCityAPI =
-//     "https://api.openweathermap.org/data/2.5/org/data/2.5/onecall?lat=" +
-//     latitude +
-//     "&lon=" +
-//     longitude +
-//     "&appid=" +
-//     apiKey;
-// }
-
-// function getCurrentPosition() {
-//   navigator.geolocation.getCurrentPosition(showPosition);
-// }
-
-// let form = document.querySelector("#temp-search");
-// form.addEventListener("submit", updateCity);
+displayForecast();
